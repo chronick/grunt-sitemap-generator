a4ce455f0413f43cc451adcfeb1871364686c96b
Nick Donohue <ndonohue@gmail.com>
2014-04-12 20:51:45 -0400

    remove some comments

--- tasks/sitemap_generator.js
+++ tasks/sitemap_generator.js
@@ -14,9 +14,6 @@ var _ = require('lodash'),
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
   grunt.registerMultiTask('sitemap_generator', 'Generate a sitemap for a dynamic application', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
